93d06838ac1469444358873a52c8fc2b6a68b8fa
theharriet <91321427+theharriet@users.noreply.github.com>
2022-02-16 06:49:25 +0000

    파이썬 if~elif~else

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-import sys
+# import sys
 
-data = sys.stdin.readline().rstrip()
-print(data)
+# data = sys.stdin.readline().rstrip()
+# print(data)
+
+score = 85
+
+if score >= 90:
+    print("A")
+elif 90 > score >= 80:
+    print("B")
+elif 80 > score >= 70:
+    print("C")
+else:
+    print("F")
