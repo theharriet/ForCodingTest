2fc1431dfad87eaf813f72061e7111a899d46ffa
theharriet <91321427+theharriet@users.noreply.github.com>
2022-03-09 13:40:41 +0000

    파이썬: 1이 될때까지 완.

--- main.py
+++ main.py
@@ -81,3 +81,15 @@ if n != 1:
     count += (n -1)
 
 print(count)
+
+#  약간 수정
+n, k = map(int, input().split())
+count = 0
+while n != 1:
+      if n % k == 0:
+          n //= k
+      else: 
+          n -= 1
+      count += 1    
+
+print(count)
