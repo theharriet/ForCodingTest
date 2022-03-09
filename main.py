fb34ca23f87ad95fc02bf444a40c8d315456fba8
theharriet <91321427+theharriet@users.noreply.github.com>
2022-03-09 13:35:50 +0000

    1이 될때까지 찐완.

--- main.py
+++ main.py
@@ -69,17 +69,15 @@ print(result)
 # 내가 푼 풀이
 n, k = map(int, input().split())
 count = 0
-while True:
-      if n % k != 0:
-          n -= 1
-          count += 1
-      else: 
+while n >= k:
+      if n % k == 0:
           n //= k
           count += 1
-      if n < k:    break;
-      
-   
-n -= 1
-count += 1
+      else: 
+          n -= 1
+          count += 1
+        
+if n != 1:      
+    count += (n -1)
 
 print(count)
