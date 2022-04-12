5f2f94a677aa54c243dfc5adf91ed17320014515
theharriet <91321427+theharriet@users.noreply.github.com>
2022-04-12 11:05:37 +0000

    nope

--- main.py
+++ main.py
@@ -21,13 +21,11 @@ dy = [1, 0, -1, 0]
 # 현재 위치
 x, y = 2, 2
 
-
-
 for i in range(4):
     # 다음 위치
     nx = x + dx[i]
     ny = y + dy[i]
     print(nx, ny)
 
-# push push baby 나를 알아줘
+# push push baby
 
