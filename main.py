6c3f248496fb84b937d236d36e1fb4acf1d8fbbd
theharriet <91321427+theharriet@users.noreply.github.com>
2022-05-26 02:37:44 +0000

    상하좌우 문제

--- main.py
+++ main.py
@@ -7,23 +7,33 @@
 #L, R, U, D는 각각 왼쪽, 오른쪽, 위, 아래로 한칸씩 이동하라는 뜻이다.
 #만약 공간을 벗어나는 움직임이 있다면 그 움직임은 무시하고 다음으로 넘어간다.
 
-# 넘어간다
+# N 입력 받기
+n = int(input())
+x, y = 1, 1
+plans = input().split()
 
+# L, R, U, D에 따른 이동 방향
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
 
+# 이동 계획을 하나씩 확인하기
+for plan in plans:
+    # 이동 후 좌표 구하기
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    # 공간을 벗어나는 경우 무시
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    # 이동 수행
+    x, y = nx, ny
 
+print(x, y)
 
-### 동, 북, 서, 남
-dx = [0, -1, 0, 1]
-dy = [1, 0, -1, 0]
+# ndb git - 4/1.java
 
-# 현재 위치
-x, y = 2, 2
-
-for i in range(4):
-    # 다음 위치
-    nx = x + dx[i]
-    ny = y + dy[i]
-    print(nx, ny)
 
 
 
